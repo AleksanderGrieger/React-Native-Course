387ba67d1d8a5975ce6300bee010a2eca2d59b22
AleksanderGrieger <61835945+AleksanderGrieger@users.noreply.github.com>
2023-04-02 20:49:29 +0200

    initial commit

---
+++ ReactNativeCourse/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+} from 'react-native';
+
+
+function App(): JSX.Element {
+
+  return (
+    <SafeAreaView>
+        <Text style={{color: 'white'}}>Hello World!</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+});
+
+export default App;
